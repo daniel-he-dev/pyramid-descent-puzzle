2677a33ede8e8795e6275c036a86521a47039f83
Daniel He <danielhe.mlp@gmail.com>
2021-03-18 16:48:27 -0500

    small adjustments

--- solve.js
+++ solve.js
@@ -12,10 +12,11 @@ axios.get(INPUT_URL).then((res) => {
 const solve = (content) => {
   //Parse Input
   const lines = content.split('\r\n');
-  const target = Number(lines.shift().split(' ')[1]);
-  lines.pop();
-  const trees = lines.map((line) => line.split(',').map((num) => new Tree(Number(num))));
+  const target = Number(lines.shift().split(' ')[1]); //Extract Target product
+  if (lines[lines.length - 1] === '') lines.pop(); //Sample Input file had an empty trailing line to remove
+  const trees = lines.map((line) => line.split(',').map((num) => new Tree(Number(num)))); //Tree instantiation
   for (let i = 0; i < trees.length - 1; i++) {
+    //Connect subtrees
     trees[i].forEach((node, idx) => {
       node.left = trees[i + 1][idx];
       node.right = trees[i + 1][idx + 1];
@@ -26,14 +27,15 @@ const solve = (content) => {
   const find = (tree, target) => {
     let newTarget = target / tree.val;
     if (!tree.left) {
+      //Check base case
       if (newTarget === 1) {
         return '';
       }
       return;
     }
-    if (newTarget < 1) return;
+    if (newTarget < 1) return; //Short-circuit optimization
 
-    let left = find(tree.left, newTarget);
+    let left = find(tree.left, newTarget); //Recursive calls
     if (left !== undefined) return 'L' + left;
     let right = find(tree.right, newTarget);
     if (right !== undefined) return 'R' + right;
